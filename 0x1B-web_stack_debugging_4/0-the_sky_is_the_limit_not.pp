0e1d090105d4c52ac9c1933e231551a22a7c3690
khawlw <142344753+khawlw@users.noreply.github.com>
2024-06-13 23:14:44 +0100

    Create 0-the_sky_is_the_limit_not.pp

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,11 @@
+# sets file limite for nginx to 2000 (default is 15)
+exec {'replace':
+  provider => shell,
+  command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
+  before   => Exec['restart'],
+}
+
+exec {'restart':
+  provider => shell,
+  command  => 'sudo service nginx restart',
+}
